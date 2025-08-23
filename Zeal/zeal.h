4ea1090bbce7806af007c32fa8c029b463fc6506
SherraBard <172213097+CoastalRedwood@users.noreply.github.com>
2025-08-23 15:08:00 -0700

    Bump to 1.2.0-alpha2 (#18)

--- Zeal/zeal.h
+++ Zeal/zeal.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <vector>
 
-#define ZEAL_VERSION "1.2.0-alpha1"
+#define ZEAL_VERSION "1.2.0-alpha2"
 #ifndef ZEAL_BUILD_VERSION               // Set by github actions
 #define ZEAL_BUILD_VERSION "UNOFFICIAL"  // Local build
 #endif
