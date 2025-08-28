cd40c4bc7456c449c13a900375bcac5b71eed08b
CoastalRedwood <172213097+CoastalRedwood@users.noreply.github.com>
2025-08-27 20:14:32 -0700

    Bump to v1.2.0-beta1

--- Zeal/zeal.h
+++ Zeal/zeal.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <vector>
 
-#define ZEAL_VERSION "1.2.0-beta0"
+#define ZEAL_VERSION "1.2.0-beta1"
 #ifndef ZEAL_BUILD_VERSION               // Set by github actions
 #define ZEAL_BUILD_VERSION "UNOFFICIAL"  // Local build
 #endif
